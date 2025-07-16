aca6b5e833e302a241619ac66aeb32426c032a96
Mohamed Saeed <mohammed453169@fci.bu.edu.eg>
2025-07-16 20:59:03 +0300

    Update sparse table.cpp

--- sparse table.cpp
+++ sparse table.cpp
@@ -1,30 +1,38 @@
-struct sparse_table {
-    vector<vector<int>> S;
-    vector<int> bin_log, a;
+struct sparse_table
+{
+    vector<vector<int>> table;
+    vector<int> log_table, a;
     int n;
 
-    void build() {
-        int cur_log = 0;
-        for (int i = 0; i < n; i++) {
-            S[i][0] = a[i];
-            if ((1 << (cur_log + 1)) <= i + 1)cur_log++;
-            bin_log[i + 1] = cur_log;
+    void build()
+    {
+        int current_log = 0;
+        for (int i = 0; i < n; i++)
+        {
+            table[i][0] = a[i];
+            if ((1 << (current_log + 1)) <= i + 1)
+                current_log++;
+            log_table[i + 1] = current_log;
         }
-        for (int k = 1; k < 20; k++) {
-            for (int i = 0; i + (1 << k) - 1 < n; i++) {
-                S[i][k] = min(S[i][k - 1], S[i + (1 << (k - 1))][k - 1]);
+        for (int j = 1; j < 20; j++)
+        {
+            for (int i = 0; i + (1 << j) - 1 < n; i++)
+            {
+                table[i][j] = min(table[i][j - 1], table[i + (1 << (j - 1))][j - 1]);
             }
         }
     }
 
-    int query(int L, int R) {
-        int k = bin_log[R - L + 1];
-        return min(S[L][k], S[R - (1 << k) + 1][k]);
+    int query(int L, int R)
+    {
+        int k = log_table[R - L + 1];
+        return min(table[L][k], table[R - (1 << k) + 1][k]);
     }
 
-    sparse_table(int n, vector<int> &a) {
-        S = vector<vector<int>>(n, vector<int>(20));
-        bin_log.resize(n + 5);
+    sparse_table(int n, vector<int> &a)
+    {
+        table = vector<vector<int>>(n, vector<int>(20));
+        log_table.resize(n + 5);
         this->a = a;
         this->n = n;
         build();
